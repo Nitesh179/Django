85db2c6089d925d1a3e8343118a1f507e0413105
Nitesh bagora <bnitesh179@gmail.com>
2023-05-17 19:27:11 +0530

    driver drowsiness

--- openai/practice/face.py
+++ openai/practice/face.py
@@ -1,29 +1,62 @@
 import cv2
-import time
 import mediapipe as mp
-
-
-cap=cv2.VideoCapture('../video/faces.mp4')
+from time import time
+import numpy as np
+import itertools 
 
 mp_drawing=mp.solutions.drawing_utils
-mpfaceMesh=mp.solutions.face_mesh
-faceMesh=mpfaceMesh.FaceMesh( max_num_faces=5)
-draw_spec=mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+mp_drawing_style=mp.solutions.drawing_styles
+mp_facemesh=mp.solutions.face_mesh
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+denormalize_coordinates = mp_drawing._normalized_to_pixel_coordinates
+mp_face=mp_facemesh.FaceMesh(max_num_faces=2,   refine_landmarks=True )
 
+cap=cv2.VideoCapture(0)
+
+# eyes index :
+left_eye_indx=list(mp_facemesh.FACEMESH_LEFT_EYE)
+left_eye_indx=set(np.ravel(left_eye_indx))
+
+right_eye_indx=list(mp_facemesh.FACEMESH_RIGHT_EYE)
+right_eye_indx=set(np.ravel(right_eye_indx))
+
+all_indx=left_eye_indx.union(right_eye_indx)
+
+# The chosen 12 points:   P1,  P2,  P3,  P4,  P5,  P6
+chosen_left_eye_idxs  = [362, 385, 387, 263, 373, 380]
+chosen_right_eye_idxs = [33,  160, 158, 133, 153, 144]
+
+all_chosen_idxs = chosen_left_eye_idxs + chosen_right_eye_idxs
+ 
 
 while True:
     ret, frames=cap.read()
-    frames=cv2.resize(frames, (1000,600))
-    rgb=cv2.cvtColor(frames, cv2.COLOR_BGR2RGB)
-    result=faceMesh.process(rgb)
+    frames=cv2.cvtColor(cv2.flip(frames, 1), cv2.COLOR_BGR2RGB)
 
-    if result.multi_face_landmarks:
-        for faces in result.multi_face_landmarks:
-            mp_drawing.draw_landmarks(frames, faces, mpfaceMesh.FACEMESH_TESSELATION, draw_spec, draw_spec)
+    face_result=mp_face.process(frames)
 
-    cv2.imshow("face detection", frames)
+    frames=cv2.cvtColor(frames, cv2.COLOR_RGB2BGR)
+    imgH, imgW, _ = frames.shape
 
+    cpy_frames=frames.copy()
+
+    if face_result.multi_face_landmarks:
+        for faceid, face_lm in enumerate(face_result.multi_face_landmarks):
+            # mp_drawing.draw_landmarks(cpy_frames, face_lm, mp_facemesh.FACEMESH_TESSELATION, landmark_drawing_spec= drawing_spec,  connection_drawing_spec=drawing_spec)
+           
+            # Get the object which holds the x, y, and z coordinates for each landmark
+            landmarks = face_lm.landmark
+            # Iterate over all landmarks.
+            # If the landmark_idx is present in either all_idxs or all_chosen_idxs,
+            # get the denormalized coordinates and plot circles at those coordinates.
+
+            for indx, landmark in enumerate(landmarks):
+                 if indx in all_chosen_idxs:
+                      pre_cord=denormalize_coordinates(landmark.x, landmark.y, imgW, imgH)
+                      cv2.circle(cpy_frames, pre_cord, 3,(255,255,0), -1)
+                      
+ 
+             
+    cv2.imshow("Face Detection", cpy_frames)
     if cv2.waitKey(2)==27:
-        break
-
-cap.release()    
+            break
---
+++ openai/practice/face2.py
@@ -0,0 +1,137 @@
+import cv2
+import mediapipe as mp
+from time import time
+import numpy as np
+from playsound import playsound
+ 
+
+mp_drawing=mp.solutions.drawing_utils
+mp_drawing_style=mp.solutions.drawing_styles
+mp_facemesh=mp.solutions.face_mesh
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+denormalize_coordinates = mp_drawing._normalized_to_pixel_coordinates
+mp_face=mp_facemesh.FaceMesh(max_num_faces=2,   refine_landmarks=True )
+
+cap=cv2.VideoCapture(0)
+ 
+# The chosen 12 points:   P1,  P2,  P3,  P4,  P5,  P6
+chosen_left_eye_idxs  = [362, 385, 387, 263, 373, 380]
+chosen_right_eye_idxs = [33,  160, 158, 133, 153, 144]
+
+all_chosen_idxs = chosen_left_eye_idxs + chosen_right_eye_idxs
+ 
+
+# calculate EAR value related functions :
+def distance(pt1, pt2):
+      dist=sum([(i-j)**2 for i, j in zip(pt1, pt2)])** 0.5
+      return dist
+
+def get_ear(landmarks, refer_indx, frame_w, frame_h):
+        try:
+            # compute horizontal distance between eyes :
+            coords_points=[]
+            for i in refer_indx:
+                  lm=landmarks[i]
+                  coord=denormalize_coordinates(lm.x, lm.y, frame_w, frame_h)
+                  coords_points.append(coord)
+             # Eye landmark (x, y)-coordinates
+            P2_P6 = distance(coords_points[1], coords_points[5])
+            P3_P5 = distance(coords_points[2], coords_points[4])
+            P1_P4 = distance(coords_points[0], coords_points[3])
+
+            # compute ear ratio becz distance function return distance :
+            ear=(P2_P6 + P3_P5) / (2.0 * P1_P4)
+
+        except:
+            ear=0.0
+            coords_points=None
+
+        return ear, coords_points      
+    
+      
+
+def calc_avg(landmarks, left_eye_idxs, right_eye_idxs, img_w, img_h):
+      left_ear, left_coordinates= get_ear(landmarks,left_eye_idxs, img_w, img_h )
+      right_ear, right_coordinates=get_ear(landmarks, right_eye_idxs, img_w, img_h)
+
+      avg_EAR=(left_ear + right_ear)/ 2.0
+
+      return avg_EAR, (left_coordinates, right_coordinates)
+
+status=""
+active=0
+drowsy=0
+color=(0,0,0)
+sleep=0
+
+while True:
+    ret, frames=cap.read()
+    if not ret:
+         break
+    
+    frames=cv2.cvtColor(cv2.flip(frames, 1), cv2.COLOR_BGR2RGB)
+
+    result=mp_face.process(frames).multi_face_landmarks
+
+    frames=cv2.cvtColor(frames, cv2.COLOR_RGB2BGR)
+
+    cpy_frames=frames.copy()
+    imgH, imgW, _ = cpy_frames.shape
+
+   
+
+    # detection works:
+    if result:
+        for indx, face_lm in enumerate(result):
+            landmarks=face_lm.landmark
+            EAR, cordinate= calc_avg(landmarks, chosen_left_eye_idxs, chosen_right_eye_idxs, imgW, imgH)
+           
+        # Print the EAR value on the custom_chosen_lmk_image.
+            cv2.putText(cpy_frames, f"EAR: {round(EAR, 2)}  ", (1, 24), cv2.FONT_HERSHEY_COMPLEX, 0.9, (0, 0, 255), 2 )  
+        #   cv2.putText(cpy_frames, f"right: {cordinate[1]}", (2, 54), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 255), 1 )  
+        #   cv2.putText(cpy_frames, f"left: {cordinate[0]}", (2, 94), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 255), 1 )  
+            
+           
+            #Checking if it is blinked
+            if(EAR<0.1):
+                sleep+=1
+                drowsy=0
+                active=0
+                if(sleep>6):
+                    # print("sleeep",sleep)
+                    playsound('../sound2.mp3')
+                    status="SLEEPING!!"
+                    color=(0,0,255)
+                
+                     
+            elif(EAR>0.1 and EAR<=0.2):
+                drowsy+=1
+                active=0
+                sleep=0
+                if(drowsy>6):
+                    # print("drowsy",drowsy)
+                    status="Drowsy !"
+                    color = (0,0,255)
+                
+            else:
+                active+=1
+                drowsy=0
+                sleep=0
+                if (active>6):
+                    # print("active",active)
+                    status="Active"
+                    color=(0,255,0)
+                
+        
+            cv2.putText(cpy_frames, status, (5,125), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color,2)
+                    
+            for indx, landmark in enumerate(landmarks):
+                if indx in all_chosen_idxs:
+                    pre_cord=denormalize_coordinates(landmark.x, landmark.y, imgW, imgH)
+                    cv2.circle(cpy_frames, pre_cord, 3,(255,255,0), -1)
+                    
+
+
+    cv2.imshow("Face Detection", cpy_frames)
+    if cv2.waitKey(2)==27:
+            break
